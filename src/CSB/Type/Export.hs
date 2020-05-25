ba58bdafc8a9e790fe3cff500cdecf22f4c39d14
Po-Yi Tsai <abt8601@protonmail.ch>
2020-05-25 12:35:26 +0800

    Add export to JSON function

---
+++ src/CSB/Type/Export.hs
@@ -0,0 +1,35 @@
+module CSB.Type.Export where
+
+import           Data.Aeson
+import qualified Data.Text                     as Text
+import           Data.Vector                    ( Vector )
+import qualified Data.Vector                   as Vector
+
+import           CSB.Type
+import           Data.Vec2
+
+instance ToJSON GameState where
+  toJSON GameState { _playerStates = Vec2 p1 p2 } = object
+    [Text.pack "playerStates" .= Array (Vector.fromList (toJSON <$> [p1, p2]))]
+
+instance ToJSON PlayerState where
+  toJSON PlayerState { _podStates = Vec2 p1 p2, _boostAvail = boostAvail, _timeout = timeout }
+    = object
+      [ Text.pack "podStates" .= Array (Vector.fromList (toJSON <$> [p1, p2]))
+      , Text.pack "boostAvail" .= toJSON boostAvail
+      , Text.pack "timeout" .= toJSON timeout
+      ]
+
+instance ToJSON PodState where
+  toJSON PodState { _position = r, _speed = v, _angle = theta, _nextcheckpointid = i, _lap = l, _shieldState = s }
+    = object
+      [ Text.pack "position" .= vec2dToJSON r
+      , Text.pack "speed" .= vec2dToJSON v
+      , Text.pack "angle" .= toJSON theta
+      , Text.pack "nextcheckpointid" .= toJSON i
+      , Text.pack "lap" .= toJSON l
+      , Text.pack "shieldState" .= toJSON s
+      ]
+   where
+    vec2dToJSON (Vec2 x y) =
+      object [Text.pack "x" .= toJSON x, Text.pack "y" .= y]
